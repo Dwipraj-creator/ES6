646c76aabfa4cf16013abe042520d70353ca1fb9
dwipraj <dwiprajdey33720@gmail.com>
2025-03-05 21:11:39 +0530

    all about ES6

--- Q12.js
+++ Q12.js
@@ -1,8 +1,8 @@
-// const people = [ { name: "Alice", address: { city: "New York", street: { name: "Broadway", number: 123 } } }, 
-//     { name: "Bob", address: { city: "Los Angeles", street: { name: "Sunset Boulevard", number: 456 } } } ];
+const people = [ { name: "Alice", address: { city: "New York", street: { name: "Broadway", number: 123 } } }, 
+    { name: "Bob", address: { city: "Los Angeles", street: { name: "Sunset Boulevard", number: 456 } } } ];
 
-//    const [{name, address:{city,street:{ name:streetName}}},{name:name1, address:{city:city1,street:{name:streetName1}}}] = people
+   const [{name, address:{city,street:{ name:streetName}}},{name:name1, address:{city:city1,street:{name:streetName1}}}] = people
 
-//     let newarr =[(`${name} lives in ${city} on ${streetName} , ${name1} lives in ${city1} on ${streetName1}`)]
-//   //  console.log(`${name1} lives in ${city1} on ${streetName1}`)
-//   console.log(newarr)
+    let newarr =[(`${name} lives in ${city} on ${streetName} , ${name1} lives in ${city1} on ${streetName1}`)]
+  //  console.log(`${name1} lives in ${city1} on ${streetName1}`)
+  console.log(newarr)
