79569639a95aa0d1ea440f9f1d0f670c9c44a5a6
dwipraj <dwiprajdey33720@gmail.com>
2025-03-06 10:44:02 +0530

    Ans about ES6

--- Q13.js
+++ Q13.js
@@ -4,11 +4,5 @@ const profile = { name: "Charlie", age: 29,
 const updates = { age: 30, address: { zipcode: "94109", country: "USA" } };
 
 
-let newUpdate = ({...profile,...updates})
-let a = ({...newUpdate,city:"Sanfravcisco"})
-console.log(a)
-// let {name , age , address} =newUpdate
-// console.log(n)
+let newUpdate = { ...profile , ...updates , address:{...profile.address,...updates.address}}
 console.log(newUpdate)
-let {name:name2 , age ,city:city1, address:{ zipcode , country}} =newUpdate
-console.log(`{name: "${name2}" , age: ${age}, city: ${city1}}`)
